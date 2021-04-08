3fcbf4c2ef36b4184c1eedf2fa3b5ad5ec2d7ff0
ThimoDewel <43780337+ThimoDewel@users.noreply.github.com>
2021-04-09 01:39:14 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,9 @@
-
 # update packages
 sudo apt update
-
 # isntall apache
 sudo apt-get install -y apache2
-
 # install php 
 sudo apt-get install -y php php-cli php-fpm php-json php-common php-mysql php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath
-
-
 # install composer
 sudo apt-get install -y unzip
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
