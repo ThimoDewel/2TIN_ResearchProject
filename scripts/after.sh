ea5076c30f648f6e439dc35915b86323fd643e56
ThimoDewel <43780337+ThimoDewel@users.noreply.github.com>
2021-05-14 17:00:10 +0200

    removed composer commands

--- scripts/after.sh
+++ scripts/after.sh
@@ -7,6 +7,5 @@ sudo rm -f /var/www/html/index.html
 sudo rm -rf /var/www/html/assets/images
 
 #composer stuff
-composer install
-
-composer dump-autoload
+#composer install
+#composer dump-autoload
